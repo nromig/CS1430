16b3fa1b96494646419d1c07226b1861240d0846
nromig <71049561+nromig@users.noreply.github.com>
2021-03-19 00:29:36 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -53,11 +53,13 @@ class YourModel(tf.keras.Model):
         #             explicitly reshape any tensors anywhere in your network.
 
         self.architecture = [
-               tf.keras.layers.Conv2D(96, 11, strides=(2,2), use_bias=True),
+               tf.keras.layers.Conv2D(32, 11, strides=(3,3), use_bias=True, activation="relu"),
                tf.keras.layers.MaxPooling2D((3,3), strides=(2,2)),
-               tf.keras.layers.Conv2D(256, 5, strides=(1,1), use_bias=True),
+               tf.keras.layers.Conv2D(64, 5, strides=(2,2), use_bias=True, activation="relu"),
                tf.keras.layers.MaxPooling2D((3,3), strides=(2,2)),
-               tf.keras.layers.Conv2D(312, 3, use_bias=True),
+               tf.keras.layers.Conv2D(64, 3, use_bias=True, activation="relu"),
+               tf.keras.layers.MaxPooling2D((3,3), strides=(2,2)),
+               tf.keras.layers.Conv2D(128, 3, use_bias=True, activation="relu"),
                tf.keras.layers.MaxPooling2D((3,3), strides=(2,2)),
                tf.keras.layers.Flatten(),
                tf.keras.layers.Dense(400, activation="relu", use_bias=True),
