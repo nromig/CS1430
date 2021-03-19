97de1eda92958ed99195554dfaf9ca94b4c8f726
nromig <71049561+nromig@users.noreply.github.com>
2021-03-18 23:45:10 -0400

    Add files via upload

--- models.py
+++ models.py
@@ -53,16 +53,16 @@ class YourModel(tf.keras.Model):
         #             explicitly reshape any tensors anywhere in your network.
 
         self.architecture = [
-               tf.keras.layers.Conv2D(32, 11, strides=(4,4), use_bias=True),
-               tf.keras.layers.Dropout(.2),
+               tf.keras.layers.Conv2D(96, 11, strides=(4,4), use_bias=True),
+               tf.keras.layers.Dropout(.1),
                tf.keras.layers.MaxPooling2D((3,3), strides=(2,2)),
-               tf.keras.layers.Conv2D(64, 5, strides=(1,1), use_bias=True),
-               tf.keras.layers.Dropout(.2),
+               tf.keras.layers.Conv2D(256, 5, strides=(1,1), use_bias=True),
+               tf.keras.layers.Dropout(.1),
                tf.keras.layers.MaxPooling2D((3,3), strides=(2,2)),
-               tf.keras.layers.Conv2D(128, 3, use_bias=True),
+               tf.keras.layers.Conv2D(312, 3, use_bias=True),
                tf.keras.layers.MaxPooling2D((3,3), strides=(2,2)),
                tf.keras.layers.Flatten(),
-               tf.keras.layers.Dense(100, activation="relu", use_bias=True),
+               tf.keras.layers.Dense(400, activation="relu", use_bias=True),
                tf.keras.layers.Dense(15, activation="softmax", use_bias=True)
         ]
 
